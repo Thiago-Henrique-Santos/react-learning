9377911efb6e3e4d2767c5f5c069a6d1b5022e96
Thiago Henrique dos Santos <72514176+Thiago-Henrique-Santos@users.noreply.github.com>
2023-12-19 11:50:42 -0300

    feat: :sparkles: Habilitando formulário de Update puxar dados conforme select
    Utilizando useState para controlar e atualizar estados dos elementos e subcomponestes do componentes update.

--- src/components/CrudSection/CrudSection.jsx
+++ src/components/CrudSection/CrudSection.jsx
@@ -1,4 +1,5 @@
 import './crud.css';
+import { useState } from 'react';
 import jsonData from './../../data/database.json';
 
 function CrudSection({type, title}) {
@@ -55,31 +56,16 @@ function Create() {
 }
 
 function Update() {
+    const [selectedPerson, setSelectedPerson] = useState(null);
+
+    function handleSelectPerson(selectedPersonId) {
+        setSelectedPerson(selectedPersonId);
+    }
+
     return (
         <form class='InsertData'>
-            <SelectPerson/>
-            <div class='DoubleInput'>
-                <div>
-                    <label>Primeiro nome:</label>
-                    <input type='text' placeholder='Nome'/>
-                </div>
-                <div>
-                    <label>Sobrenome:</label>
-                    <input type='text' placeholder='Sobrenome'/>
-                </div>
-            </div>
-            <div>
-                <label>Data de nascimento:</label>
-                <input type='date'/>
-            </div>
-            <div>
-                <label>Altura:</label>
-                <input type='text' placeholder='Ex: 1.80'/>
-            </div>
-            <div>
-                <label>Peso:</label>
-                <input type='text' placeholder='Ex: 79.1'/>
-            </div>
+            <SelectPerson onSelectPerson={handleSelectPerson}/>
+            <DisplayInformation selectedPersonId={selectedPerson}/>
         </form>
     );
 }
@@ -112,25 +98,97 @@ function SearchPerson() {
     );
 }
 
-function SelectPerson () {
+function SelectPerson ({onSelectPerson}) {
+    function handleChange (event) {
+        const selectedPersonId = event.target.value;
+        onSelectPerson(selectedPersonId);
+    }
+
     let everyone = [];
     jsonData.everyone.forEach((person) => {
         everyone.push(person);
     });
 
     let options = everyone.map(person =>
-        <option value={person.id}>{person.firstName} {person.lastName}</option>
+        <option key={person.id} value={person.id}>
+            {person.firstName} {person.lastName}
+        </option>
     );
 
     return (
         <div class='InsertData'>
             <label>Nome completo:</label>
-            <select>
-                <option value='default'>Selecione</option>
+            <select onChange={handleChange}>
+                <option key={0} value='default'>Selecione</option>
                 {options}
             </select>
         </div>
     );
+}
+
+function DisplayInformation({selectedPersonId}){
+    let selectedPerson;
+    jsonData.everyone.forEach((person) => {
+        if(person.id === selectedPersonId) {
+            selectedPerson = person;
+        }
+    });
+
+    if (selectedPerson) {
+        return (
+            <>
+                <div class='DoubleInput'>
+                    <div>
+                        <label>Primeiro nome:</label>
+                        <input type='text' placeholder='Nome' value={selectedPerson.firstName}/>
+                    </div>
+                    <div>
+                        <label>Sobrenome:</label>
+                        <input type='text' placeholder='Sobrenome' value={selectedPerson.lastName}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Data de nascimento:</label>
+                    <input type='date' value={selectedPerson.birthdate.slice(0, 10)}/>
+                </div>
+                <div>
+                    <label>Altura:</label>
+                    <input type='text' placeholder='Ex: 1.80' value={selectedPerson.height}/>
+                </div>
+                <div>
+                    <label>Peso:</label>
+                    <input type='text' placeholder='Ex: 79.1' value={selectedPerson.weight}/>
+                </div>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <div class='DoubleInput'>
+                    <div>
+                        <label>Primeiro nome:</label>
+                        <input type='text' placeholder='Nome' value={''}/>
+                    </div>
+                    <div>
+                        <label>Sobrenome:</label>
+                        <input type='text' placeholder='Sobrenome' value={''}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Data de nascimento:</label>
+                    <input type='date' value={''}/>
+                </div>
+                <div>
+                    <label>Altura:</label>
+                    <input type='text' placeholder='Ex: 1.80' value={''}/>
+                </div>
+                <div>
+                    <label>Peso:</label>
+                    <input type='text' placeholder='Ex: 79.1' value={''}/>
+                </div>
+            </>
+        );
+    }
 }
 
 function ShowInformation({firstName, lastName, birthdate, height, weight}) {
